7c9db609281c87a559de3c08ddfa71dabe3afa1a
Karan Attri <karanattri@Karans-MacBook-Pro.local>
2024-08-23 17:32:49 +0530

    6.2 useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,26 @@
-import React, { useState,useEffect } from 'react'
+import React, { useState, useEffect, useMemo } from 'react'
 
 
 
 function App() {
 
-  const[id,setId] = useState(1)
+  const[count,setCount] = useState(1)
+  const[inputValue, setInputValue] = useState(1)
 
-  // const[todos, setTodos] = useState([])
-  // useEffect(()=> {
-  //  fetch("https://sum-server.100xdevs.com/todos").then(
-  //   async function(res) {
-  //     let json = await res.json();
-  //     setTodos(json.todos);
-  //   })
-  // },[])
-  function changeId(id) {
-    setId(id);
-  }
+// useMemo
+  let total = useMemo(()=>{
+    return (parseInt(inputValue) * ( parseInt(inputValue) + 1) ) / 2;
+  },[inputValue])
+
   return (
     <div>
-        <button onClick={function () {setId(1)}}>1</button>
-        <button onClick={function () {setId(2)}}>2</button>
-        <button onClick={function () {setId(3)}}>3</button>
-        <button onClick={function () {setId(4)}}>4</button>
-        <Todo id={id}/>
+      <input onChange={(e)=> setInputValue(e.target.value || 0)}></input>
+      <br></br>
+      <div>{total}</div>
+      <button onClick={()=>setCount(count+1)}>Counter: {count}</button>
     </div>
   )
 }
 
-function Todo ({id}) {
-  const[ todo, setTodos ] = useState({})
-  useEffect(()=>{
-    fetch(`https://sum-server.100xdevs.com/todo?id=${id}`).then(
-      async function(response) {
-        const json = await response.json();
-        setTodos(json.todo)
-        // setTodos({...todos,json.todo})
-      }
-    )
-  },[id])
-  return (
-    <div>
-      <h1>{todo.title}</h1>
-      <h5>{todo.desciption}</h5>
-    </div>
-  )
-}
 
 export default App;
