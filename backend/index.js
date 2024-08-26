f3a105eb5197791fb18ed89087c7e91d69bb10ab
Karan Attri <karanattri@Karans-MacBook-Pro.local>
2024-08-26 17:23:10 +0530

    5.2 How to use Zod

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@
 // with express.json() middleware
 
 const express =  require('express');
+const { createTodo, updateTodo } = require('./types');
 
 const app = express();
 
@@ -18,9 +19,28 @@ app.get('/todos',function(req,res){
 })
 
 app.post('/todo',function(req,res){
-    
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload)
+
+    if(!parsedPayload.success){
+        res.status(411).json({
+            message:"Wrong inputs"
+        })
+        return;
+    }
+
+    // put in mongoDb if everything is fine
 })
 
 app.put('/completed',function(req,res){
-    
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload);
+    if(!parsedPayload.success){
+        res.status(414).json({
+            message:"Wrong Input"
+        })
+        return;
+    }
+    // mongodb
+
 })
