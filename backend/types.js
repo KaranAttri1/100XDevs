d452cb0da40c09059b7cb51e2a1b1ed715a0b115
Karan Attri <karanattri@Karans-MacBook-Pro.local>
2024-08-26 14:54:41 +0530

    Zod: Schema for object types expected in the api calls

---
+++ backend/types.js
@@ -0,0 +1,30 @@
+const zod = require('zod');
+
+ /*
+
+    {
+        title: string
+        description: string
+    }
+
+    {
+        id: string
+    }
+
+
+ */
+
+const createTodo = zod.object({
+    title: zod.string(),
+    description: zod.string()
+})
+
+const updateTodo = zod.object({
+    id: zod.string(),  
+})
+
+// way to export these
+module.exports = {
+    createTodo,
+    updateTodo
+}
