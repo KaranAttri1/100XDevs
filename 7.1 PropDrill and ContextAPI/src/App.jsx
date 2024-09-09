105936d8a6f189013afb7be456c3597b143dfb45
Karan Attri <karanattri@Karans-MacBook-Pro.local>
2024-09-09 11:48:52 +0530

    What is PropDrilling

--- 7.1 PropDrill and ContextAPI/src/App.jsx
+++ 7.1 PropDrill and ContextAPI/src/App.jsx
@@ -8,9 +8,33 @@ function App() {
 
   return (
     <>
-    
+      <Count count={count}></Count>
     </>
   )
+}
+
+function Count({count,setCount}) {
+return (
+  <div>
+    Count:{count}
+    {/* Example of PropDrilling here is passing setCount to Count, 
+        Count doesnt require setCount but we passed it bcz its child Buttons needed it  */}
+    <Buttons count={count} setCount={setCount}></Buttons>  
+    </div>
+)
+}
+
+function Buttons({count,setCount}){
+    return(
+      <>
+      <button onClick={()=>setCount(count + 1)}>
+          Increase
+      </button> 
+      <button onClick={()=>setCount(count - 1)}>
+          Decrease
+      </button>
+      </>
+    )
 }
 
 export default App
