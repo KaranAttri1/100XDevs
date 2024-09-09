43342ee130d016003d87ad4667996ebc129ca06c
Karan Attri <karanattri@Karans-MacBook-Pro.local>
2024-09-09 08:55:33 +0530

    How to not use usenavigate()

--- routing/src/App.jsx
+++ routing/src/App.jsx
@@ -2,16 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { BrowserRouter, Route, Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes, useNavigate} from 'react-router-dom'
 import Dashboard from '../components/Dashboard'
 import Landing from '../components/Landing'
 function App() {
 
+  // *** you can only declare useNavigate statement inside BrowserRouter, so here it is WRONG ***
+  const navigate = useNavigate(); 
+
   return (
     <BrowserRouter>
-        {/* this navbar stays at the same place even if route changes */}
-        <div style={{background:'black'}}>
-          This is topbar
+        <div>
+          <button onClick={()=>navigate('/')}>Go to Landing </button>
+          <button onClick={()=>navigate('/dashboard')}>Go to Dashboard </button>
         </div>
         <Routes>
             <Route path="/dashboard" element={<Dashboard/>}></Route>
