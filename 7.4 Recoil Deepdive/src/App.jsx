00421953fd1a637a36d7369de4e9375be225c8d7
Karan Attri <karanattri@Karans-MacBook-Pro.local>
2024-09-12 14:30:14 +0530

    7.4 Incorrect way to get async data queries

--- 7.4 Recoil Deepdive/src/App.jsx
+++ 7.4 Recoil Deepdive/src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
-import {useRecoilState,useRecoilValue,RecoilRoot} from 'recoil'
+import {useRecoilState,useRecoilValue,RecoilRoot,useSetRecoilState} from 'recoil'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { jobsAtom, meSum, messagesAtom, networkAtom, notificationsAtom } from './store/atoms/navbar'
+import { meSum, navbarAtom } from './store/atoms/navbar'
+import { useEffect } from 'react'
+import axios from 'axios';
 
 function App() {
   return(
@@ -15,22 +17,57 @@ function App() {
 
 
 function MainApp() {
-  const network = useRecoilValue(networkAtom);
-  const jobs = useRecoilValue(jobsAtom);
-  const [messaging,setMessaging] = useRecoilState(messagesAtom);
-  const notifications = useRecoilValue(notificationsAtom);
+  //https://sum-server.100xdevs.com/notifications 
+
+  // returns {  'network':6
+  //            'jobs':3
+  //            'messaging':7
+  //            'notifications':9    
+  //           }
+
+  const [navbar,setNavbar] = useRecoilState(navbarAtom)
+
+  // const setNetwork = useSetRecoilState(networkAtom);
+  // const setJobs = useSetRecoilState(jobsAtom);
+  // const setMessaging = useSetRecoilState(messagesAtom);
+  // const setNotifications = useSetRecoilState(notificationsAtom);
+
+  useEffect(()=>{
+    // *** USING FETCH
+    // fetch('http://localhost:8080/notifications').then(
+    //   async(res)=>{
+    //     var data = await res.json();
+    //     setNavbar(data)
+    //     // setNetwork(data.network);
+    //     // setJobs(data.jobs);
+    //     // setMessaging(data.messaging);
+    //     // setNotifications(data.notifications);
+    //   })
+
+
+    // USING AXIOS
+    axios.get('http://localhost:8080/notifications').then(
+      (res)=>{
+        setNavbar(res.data);
+      }
+    )
+  },[])
+  // const network = useRecoilValue(networkAtom);
+  // const jobs = useRecoilValue(jobsAtom);
+  // const [messaging,setMessaging] = useRecoilState(messagesAtom);
+  // const notifications = useRecoilValue(notificationsAtom);
   const meSelector = useRecoilValue(meSum)
   return (
     <>
       <button>Home</button>
 
-      <button>My network ({network > 99 ? '99+' : network})</button>
-      <button>Jobs ({jobs})</button>
-      <button>Messaging ({messaging})</button>
-      <button>Notifications ({notifications})</button>
+      <button>My network ({navbar.network > 99 ? '99+' : navbar.network})</button>
+      <button>Jobs ({navbar.jobs})</button>
+      <button>Messaging ({navbar.messaging})</button>
+      <button>Notifications ({navbar.notifications})</button>
 
       <button onClick={()=>{
-        setMessaging(messaging + 1)
+        setNavbar({...navbar,messaging: navbar.messaging + 1})
       }}>Me ({meSelector})</button>
     </>
   )
--- 7.4 Recoil Deepdive/src/store/atoms/navbar.jsx
+++ 7.4 Recoil Deepdive/src/store/atoms/navbar.jsx
@@ -1,24 +1,34 @@
 import {atom,selector} from 'recoil'
 
-export const networkAtom = atom({
-    key:"networkAtom",
-    default:102
+export const navbarAtom = atom({
+    key:"navbarAtom",
+    default:{
+        network:6,
+        jobs:3,
+        messaging:7,
+        notifications:9
+    }
 })
 
-export const jobsAtom = atom({
-    key:"jobsAtom",
-    default:0
-})
+// export const networkAtom = atom({
+//     key:"networkAtom",
+//     default:102
+// })
 
-export const messagesAtom = atom({
-    key:"messagesAtom",
-    default:0
-})
+// export const jobsAtom = atom({
+//     key:"jobsAtom",
+//     default:0
+// })
 
-export const notificationsAtom = atom({
-    key:"notificationsAtom",
-    default:102
-})
+// export const messagesAtom = atom({
+//     key:"messagesAtom",
+//     default:0
+// })
+
+// export const notificationsAtom = atom({
+//     key:"notificationsAtom",
+//     default:102
+// })
 
 
 // Selector => here show sum of network + notifications over Me button
@@ -26,6 +36,7 @@ export const notificationsAtom = atom({
 export const meSum = selector({
     key:"meSelector",
     get:({get}) => {
-        return get(networkAtom) + get(notificationsAtom) + get(jobsAtom) + get(messagesAtom);
+        const navbar = get(navbarAtom)
+        return navbar.network + navbar.jobs + navbar.messaging + navbar.notifications;
     }
 })
